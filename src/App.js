fc7e0c00938e609684619e6c6b3d68a354b63235
sundongdong <793328662@qq.com>
2018-04-19 11:14:30 +0800

    等待尝试路由

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './reset.scss';
-//@import('./reset')
+import { Grid, Navbar, Jumbotron, Button, Nav, NavItem} from 'react-bootstrap';
+import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          孙冬冬.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Navbar inverse fixedTop>
+                    <Grid>
+                        <Navbar.Header>
+                            <Navbar.Brand>
+                                <a href="/">React App</a>
+                            </Navbar.Brand>
+                            <Navbar.Toggle />
+                        </Navbar.Header>
+                        <Nav bsStyle="pills" activeKey={1}>
+                            <NavItem eventKey={1} href="/home">导航条目 1 的内容</NavItem>
+                            <NavItem eventKey={2} title="Item">导航条目 2 的内容</NavItem>
+                            <NavItem eventKey={3} disabled>导航条目 3 的内容</NavItem>
+                        </Nav>
+                    </Grid>
+                </Navbar>
+                <Jumbotron>
+                    <Grid>
+                        <h1>Welcome to React</h1>
+                        <p>
+                            <Button
+                                bsStyle="success"
+                                bsSize="large"
+                                href="http://react-bootstrap.github.io/components.html"
+                                target="_blank">
+                                View React Bootstrap Docs
+                            </Button>
+                        </p>
+                    </Grid>
+                </Jumbotron>
+            </div>
+        );
+    }
 }
 
 export default App;
